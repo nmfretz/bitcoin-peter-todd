a2e626d7ebb13f30d0eec76c9f296ebf6813dd8b
Peter Todd <pete@petertodd.org>
2024-01-19 19:37:10 +0000

    Apply full-rbf patches to v26.0
    Specifically:
    
        git diff v25.1 petertodd/full-rbf-v25.1

--- src/protocol.h
+++ src/protocol.h
@@ -301,6 +301,8 @@ enum ServiceFlags : uint64_t {
     // collisions and other cases where nodes may be advertising a service they
     // do not actually support. Other service bits should be allocated via the
     // BIP process.
+
+    NODE_FULL_RBF = (1 << 26),
 };
 
 /**
@@ -356,6 +358,14 @@ static inline bool HasAllDesirableServiceFlags(ServiceFlags services)
 static inline bool MayHaveUsefulAddressDB(ServiceFlags services)
 {
     return (services & NODE_NETWORK) || (services & NODE_NETWORK_LIMITED);
+}
+
+/**
+ * Checks if a peer with the given service flags enables fullrbf.
+ */
+static inline bool HasFullRBFServiceFlag(ServiceFlags services)
+{
+    return (services & NODE_FULL_RBF);
 }
 
 /** A CService with information about it as peer */
